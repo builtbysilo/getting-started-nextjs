2031ac6abb9dfdaad0fa5c7859247d4fd5b68801
builtbysilo <cody@builtbysilo.com>
2023-10-11 10:46:10 -0500

    Configured

--- pages/api/predictions/index.js
+++ pages/api/predictions/index.js
@@ -1,8 +1,23 @@
+import multer from "multer";
 import Replicate from "replicate";
+import fetch from "node-fetch";
 
 const replicate = new Replicate({
   auth: process.env.REPLICATE_API_TOKEN,
 });
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+  },
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default async function handler(req, res) {
   if (!process.env.REPLICATE_API_TOKEN) {
@@ -11,21 +26,95 @@ export default async function handler(req, res) {
     );
   }
 
-  const prediction = await replicate.predictions.create({
-    // Pinned to a specific version of Stable Diffusion
-    // See https://replicate.com/stability-ai/sdxl
-    version: "8beff3369e81422112d93b89ca01426147de542cd4684c244b673b105188fe5f",
+  const multerHandler = upload.fields([
+    {name: "swap_image"},
+    {name: "target_image"},
+  ]);
 
-    // This is the text prompt that will be submitted by a form on the frontend
-    input: { prompt: req.body.prompt },
+  await new Promise((resolve, reject) => {
+    multerHandler(req, res, (err) => {
+      if (err) {
+        console.error("Multer Error:", err);
+        res.statusCode = 500;
+        res.end(JSON.stringify({detail: err.message}));
+        return reject(err);
+      }
+      resolve();
+    });
   });
 
-  if (prediction?.error) {
-    res.statusCode = 500;
-    res.end(JSON.stringify({ detail: prediction.error }));
-    return;
+  const swapImage = req.files["swap_image"] && req.files["swap_image"][0];
+  const targetImage = req.files["target_image"] && req.files["target_image"][0];
+
+  const targetImageDataUrl = `data:${
+    targetImage.mimetype
+  };base64,${targetImage.buffer.toString("base64")}`;
+  const swapImageDataUrl = `data:${
+    swapImage.mimetype
+  };base64,${swapImage.buffer.toString("base64")}`;
+
+  if (!swapImage || !targetImage) {
+    res.statusCode = 400;
+    return res.end(
+      JSON.stringify({
+        detail: "Both swap image and target image must be uploaded.",
+      })
+    );
   }
 
-  res.statusCode = 201;
-  res.end(JSON.stringify(prediction));
+  const model =
+    "vbarter/faceswap:a2931eb76fdde4505638e596d0b452535e28c94b02a418c4f95accc9084b1eed";
+
+  try {
+    // First API call
+    const input = {
+      input: {
+        swap_image: swapImageDataUrl,
+        target_image: targetImageDataUrl,
+      },
+    };
+
+    const output = await replicate.run(model, input);
+
+    if (output?.error) {
+      res.statusCode = 500;
+      res.end(JSON.stringify({detail: output.error}));
+      return;
+    }
+
+    // Assuming the URL of the image from the response is stored in output.image_url
+    const response = await fetch(output);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch image from ${output}`);
+    }
+    const imageBuffer = await response.buffer();
+    const imageUrlData = `data:image/jpeg;base64,${imageBuffer.toString(
+      "base64"
+    )}`;
+
+    const model2 =
+      "sczhou/codeformer:7de2ea26c616d5bf2245ad0d5e24f0ff9a6204578a5c876db53142edd9d2cd56";
+
+    // Second API call
+    const secondInput = {
+      input: {
+        image: imageUrlData,
+      },
+    };
+
+    const secondOutput = await replicate.run(model2, secondInput);
+
+    if (secondOutput?.error) {
+      res.statusCode = 500;
+      res.end(JSON.stringify({detail: secondOutput.error}));
+      return;
+    }
+
+    res.statusCode = 201;
+    res.end(JSON.stringify(secondOutput));
+  } catch (err) {
+    console.error("Error:", err);
+    res.statusCode = 500;
+    res.end(JSON.stringify({detail: err.message}));
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,89 @@
-import { useState } from "react";
+import {useState} from "react";
 import Head from "next/head";
 import Image from "next/image";
-
-const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 export default function Home() {
   const [prediction, setPrediction] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [imagePreview, setImagePreview] = useState(null);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const response = await fetch("/api/predictions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt: e.target.prompt.value,
-      }),
-    });
-    let prediction = await response.json();
-    if (response.status !== 201) {
-      setError(prediction.detail);
-      return;
-    }
-    setPrediction(prediction);
+  //? Target Image Path
+  const TARGET_IMAGE_PATH = "/astro.jpg";
 
-    while (
-      prediction.status !== "succeeded" &&
-      prediction.status !== "failed"
-    ) {
-      await sleep(1000);
-      const response = await fetch("/api/predictions/" + prediction.id);
-      prediction = await response.json();
-      if (response.status !== 200) {
-        setError(prediction.detail);
-        return;
-      }
-      console.log({ prediction });
-      setPrediction(prediction);
+  //? Image Preview
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
     }
   };
+
+  //? Generate Image
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+
+    const file = e.target.img.files[0];
+    if (!file) {
+      setError("No file selected");
+      setLoading(false);
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("swap_image", file);
+
+    const targetImageResponse = await fetch(TARGET_IMAGE_PATH);
+    const targetImageBlob = await targetImageResponse.blob();
+    formData.append("target_image", targetImageBlob, TARGET_IMAGE_PATH);
+
+    const response = await fetch("/api/predictions", {
+      method: "POST",
+      body: formData,
+    });
+
+    let responseData = await response.json();
+
+    if (response.status !== 201) {
+      setError(responseData.detail);
+      setLoading(false);
+      return;
+    }
+
+    setPrediction(responseData);
+    setLoading(false);
+  };
+
+  //? Download Image
+  const handleDownload = async () => {
+    const imageSrc = prediction; // Adjust this path
+
+    // Fetch the image as a Blob
+    const response = await fetch(imageSrc);
+    if (!response.ok) {
+      throw new Error("Failed to fetch image for download.");
+    }
+    const blob = await response.blob();
+
+    // Create an Object URL from the Blob
+    const url = URL.createObjectURL(blob);
+
+    // Use a temporary anchor tag to trigger the download
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "desired-filename.jpg"; // Adjust the desired filename
+    a.click();
+
+    // Cleanup (optional but recommended)
+    URL.revokeObjectURL(url);
+  };
+
+  console.log(prediction);
 
   return (
     <div className="container max-w-2xl mx-auto p-5">
@@ -49,39 +92,94 @@ export default function Home() {
       </Head>
 
       <h1 className="py-6 text-center font-bold text-2xl">
-        Dream something with{" "}
-        <a href="https://replicate.com/stability-ai/sdxl?utm_source=project&utm_project=getting-started">
-          SDXL
-        </a>
+        Cody s DIY AI Face Swap
       </h1>
 
-      <form className="w-full flex" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="flex-grow"
-          name="prompt"
-          placeholder="Enter a prompt to display an image"
-        />
-        <button className="button" type="submit">
-          Go!
+      <form className="w-full flex flex-col" onSubmit={handleSubmit}>
+        <div class="flex items-center justify-center w-full">
+          <label
+            for="dropzone-file"
+            class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+          >
+            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg
+                class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 16"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                />
+              </svg>
+              <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                <span class="font-semibold">Click to upload</span> or drag and
+                drop
+              </p>
+              <p class="text-xs text-gray-500 dark:text-gray-400">
+                SVG, PNG, JPG or GIF (MAX. 800x400px)
+              </p>
+            </div>
+            <input
+              id="dropzone-file"
+              type="file"
+              class="hidden"
+              name="img"
+              onChange={handleImageChange}
+            />
+          </label>
+        </div>
+
+        <button className="button" type="submit" disabled={loading}>
+          {loading ? "Processing..." : "Go!"}
         </button>
       </form>
+
+      {/* Display image preview */}
+      {imagePreview && (
+        <div className="mt-4">
+          <Image
+            src={imagePreview}
+            alt="Image Preview"
+            width={200}
+            height={200}
+          />
+        </div>
+      )}
+
+      {/* Display spinner when loading */}
+      {loading && (
+        <div className="my-5 text-center">
+          <div className="spinner" />
+        </div>
+      )}
 
       {error && <div>{error}</div>}
 
       {prediction && (
         <>
-          {prediction.output && (
+          {prediction && (
             <div className="image-wrapper mt-5">
-              <Image
-                fill
-                src={prediction.output[prediction.output.length - 1]}
-                alt="output"
-                sizes="100vw"
-              />
+              <Image src={prediction} alt="output" width={500} height={500} />
             </div>
           )}
-          <p className="py-3 text-sm opacity-50">status: {prediction.status}</p>
+          {prediction && (
+            <button
+              onClick={handleDownload}
+              className="mt-3 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-200"
+            >
+              Download Image
+            </button>
+          )}
+
+          <p className="py-3 text-sm opacity-50">
+            status: {prediction.status} ||{" "}
+          </p>
         </>
       )}
     </div>
